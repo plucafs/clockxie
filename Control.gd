ba0554b5223b6aa63185e21fe79174bfe6c12fbd
Luca Poli <86297633+plucafs@users.noreply.github.com>
2021-06-22 13:30:07 +0200

    Changed the objects alignment

--- Control.gd
+++ Control.gd
@@ -3,6 +3,6 @@ extends Control
 var date_time = str(OS.get_datetime())
 
 func _process(_delta):
-	$Clock/Hour.text = str(OS.get_time()['hour']) + ' h'
-	$Clock/Minute.text = str(OS.get_time()['minute']) + ' m'
-	$Clock/Seconds.text = str(OS.get_time()['second']) + ' s'
+	$HBoxContainer/Clock/Hour.text = str(OS.get_time()['hour']) + ' h'
+	$HBoxContainer/Clock/Minute.text = str(OS.get_time()['minute']) + ' m'
+	$HBoxContainer/Clock/Seconds.text = str(OS.get_time()['second']) + ' s'
